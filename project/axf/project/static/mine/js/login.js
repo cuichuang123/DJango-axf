d08d5cbab4844472a6a15efbf12ac914ae311d5b
cuic <516766219@qq.com>
2018-06-28 13:27:09 +0800

    上线 www.wccxpp.cn

---
+++ project/axf/project/static/mine/js/login.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+   $("#smsBtn").bind("click", function () {
+       //告诉后台发送验证码
+       data = {"mobile":$("#phone").val()};
+      $.ajax({
+          url:"/verifycode/",
+          data:data,
+          type:"get",
+          dataType:"json",
+          success:function (data, status) {
+              console.log(data, status);
+          }
+      })
+   }) ;
+});
